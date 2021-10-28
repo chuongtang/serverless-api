da7ba73e24b6c51d8afdbba57390759f7873a623
chuongtang <chuong.tang@aol.com>
2021-10-27 23:23:54 -0600

    added canvas components

--- frontend/react-ui/src/App.jsx
+++ frontend/react-ui/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   let results = [];
 
   async function getData() {
-    const res = await fetch("http://127.0.0.1:8787");
+    const res = await fetch("https://serverless-api.chuongtang.workers.dev");
     results = await res.json();
     setCoins(results.data);
     console.log("Here are they", results.data);
@@ -34,50 +34,50 @@ const App = () => {
         <Header />
 
         {coins ? (
-      
-        <div>
-          <div className="background">
-        <div className="cube"></div>
-        <div className="cube"></div>
-        <div className="cube"></div>
-        <div className="cube"></div>
-        <div className="cube"></div>
+
+          <div>
+            <div className="background">
+              <div className="cube"></div>
+              <div className="cube"></div>
+              <div className="cube"></div>
+              <div className="cube"></div>
+              <div className="cube"></div>
+            </div>
+            <div>
+              <div className="updTime">Last updated on:  🌠 {updateTime}</div>
+              <Container>
+                <Table striped bordered hover >
+                  <thead >
+                    <tr>
+                      <th>Current Rank</th>
+                      <th>Symbol</th>
+                      <th>Name</th>
+                      <th>Price(USD)</th>
+                      <th>% Change is last hour</th>
+                      <th>Total Supply</th>
+                      <th>Volume_Change in 24h</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* loop over the coins */}
+                    {coins.map((coin, index) => (
+                      <tr key={index}>
+                        <td>{coin.cmc_rank}</td>
+                        <td>{coin.symbol}</td>
+                        <td>{coin.slug}</td>
+                        <td>{coin.quote.USD.price}</td>
+                        <td>{coin.quote.USD.percent_change_1h}</td>
+                        <td>{coin.total_supply}</td>
+                        <td>{coin.quote.USD.volume_change_24h}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </Container>
+            </div>
+          </div>
+        ) : (<Loader />)}
       </div>
-      <div>
-        <div className="updTime">Last updated on:  🌠 {updateTime}</div>
-        <Container>
-          <Table striped bordered hover >
-            <thead >
-              <tr>
-                <th>Current Rank</th>
-                <th>Symbol</th>
-                <th>Name</th>
-                <th>Price(USD)</th>
-                <th>% Change is last hour</th>
-                <th>Total Supply</th>
-                <th>Volume_Change in 24h</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* loop over the coins */}
-              {coins.map((coin, index) => (
-                <tr key={index}>
-                  <td>{coin.cmc_rank}</td>
-                  <td>{coin.symbol}</td>
-                  <td>{coin.slug}</td>
-                  <td>{coin.quote.USD.price}</td>
-                  <td>{coin.quote.USD.percent_change_1h}</td>
-                  <td>{coin.total_supply}</td>
-                  <td>{coin.quote.USD.volume_change_24h}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </Container>
-      </div>
-      </div>
-      ) : (<Loader />)}
-    </div>
     </div >
   )
 }
--- frontend/react-ui/src/components/Header.jsx
+++ frontend/react-ui/src/components/Header.jsx
@@ -1,5 +1,9 @@
 import { Navbar, Container, Offcanvas, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
-
+import Tlc from '../images/TLC.png'
+import Vite from '../images/Vite.png'
+import Workers from '../images/Workers.png'
+import ReactBS from '../images/react-bootstrap.png'
+import CoinMarket from '../images/CoinMarket.png'
 
 const Header = () => {
 
@@ -21,27 +25,19 @@ const Header = () => {
             <Nav className="justify-content-end flex-grow-1 pe-3">
               <Nav.Link href="https://www.pwc.com/us/en/industries/financial-services/fintech/bitcoin-blockchain-cryptocurrency.html#:~:text=Bitcoin%20is%20the%20name%20of%20the%20best-known%20cryptocurrency%2C,the%20transfer%20of%20funds.%20What%20is%20blockchain%20technology%3F" target="_blank" rel="noopener noreferrer">💴 Technologies behind Crypto</Nav.Link>
               <Nav.Link href="https://cryptohelp.com/" target="_blank" rel="noopener noreferrer" > 📈 Crypto-Invest</Nav.Link>
+              <hr />
               <NavDropdown title="This web page was built by : " id="offcanvasNavbarDropdown">
-                <NavDropdown.Item href="#action3">VITE</NavDropdown.Item>
-                <NavDropdown.Item href="#action3">React-Bootstrap </NavDropdown.Item>
-                <NavDropdown.Item href="#action4">Cloudflare serverless, </NavDropdown.Item>
-                <NavDropdown.Item href="#action4">CoinmarketCap API</NavDropdown.Item>
+                <NavDropdown.Item href="#action3"><img  src={Vite} alt="Vite Logo" className="logo"/> <a href={'https://vitejs.dev'} target="_blank" rel="noopener noreferrer">VITEjs</a></NavDropdown.Item>
+                <NavDropdown.Item href="#action3"><img  src={ReactBS} alt="React Bootstrap Logo" className="logo"/>  <a href={'https://react-bootstrap.github.io'} target="_blank" rel="noopener noreferrer"> React-Bootstrap</a></NavDropdown.Item>
+                <NavDropdown.Item href="#action4"><img  src={Workers} alt="Workers Logo" className="logo"/>  <a href={'https://workers.cloudflare.com'} target="_blank" rel="noopener noreferrer"> Cloudflare Serverless</a></NavDropdown.Item>
+                <NavDropdown.Item href="#action4"><img  src={CoinMarket} alt="CoinMarket Logo" className="logo"/> <a href={'https://coinmarketcap.com/api/'} target="_blank" rel="noopener noreferrer">CoinMarketCap API</a></NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action5">
-                  a blockchain explorer
+                 and.. <img  src={Tlc} alt="Chuong Favicon" className="logo"/> , <a href={'https://chuongtang.github.io/aboutme' } target="_blank" rel="noopener noreferrer">a developer, blockchain explorer</a> 
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-            <iframe content-security-policy={{ 'frame-ancestors': 'self' }} style={{ 'height': '25vh' }} loading="eager" title="Preview Crypto detail." src="https://coinmarketcap.com/currencies/uniswap/"></iframe>
+          
           </Offcanvas.Body>
         </Navbar.Offcanvas>
       </Container>
